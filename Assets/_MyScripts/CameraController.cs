6803eae8f8e70a9c83109bf508caa9bc05397082
AydanH <tiddle97@gmail.com>
2019-07-12 21:32:06 +1000

    dynamic camera restored

--- Assets/_MyScripts/CameraController.cs
+++ Assets/_MyScripts/CameraController.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private GameObject mainCamera;
-    
+    [SerializeField] private Transform mainCamera;
+    [SerializeField] private Transform player1;
+    [SerializeField] private Transform player2;
+
+    private Vector3 offset = new Vector3(0,20,-20);
+
+    private Vector3 midpoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +20,10 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        offset = (play);
+        midpoint = (player1.position + player2.position )/2;
+        mainCamera.SetPositionAndRotation(midpoint+offset,mainCamera.rotation);
+        mainCamera.LookAt(midpoint);
         
     }
 }

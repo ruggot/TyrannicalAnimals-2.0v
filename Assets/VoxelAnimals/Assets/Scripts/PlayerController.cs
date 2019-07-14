433c32f77f1b79dd6e71b55a6a022176966ae51f
Aydan Heeley <tiddle97@gmail.com>
2019-07-14 22:41:15 +1000

    Multiple controller types functioning for input
    Added rough NSPC controller mapping and a way to distinguish controllers per character in the editor

--- Assets/VoxelAnimals/Assets/Scripts/PlayerController.cs
+++ Assets/VoxelAnimals/Assets/Scripts/PlayerController.cs
@@ -10,9 +10,10 @@ public class PlayerController : MonoBehaviour
     public float timeBeforeNextJump = 1.2f;
     private float canJump = 0f;
     [SerializeField] private string player;
+    [SerializeField] private string gPad;
     Animator anim;
     Rigidbody rb;
-    
+
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -26,8 +27,8 @@ public class PlayerController : MonoBehaviour
 
     void ControllPlayer()
     {
-        float moveHorizontal = Input.GetAxisRaw("J" + player + "_Horizontal_360");
-        float moveVertical = Input.GetAxisRaw("J" + player + "_Vertical_360");
+        float moveHorizontal = Input.GetAxisRaw("J" + player + "_Horizontal_" + gPad);
+        float moveVertical = Input.GetAxisRaw("J" + player + "_Vertical_" + gPad);
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
@@ -36,21 +37,22 @@ public class PlayerController : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
             anim.SetInteger("Walk", 1);
         }
-        else {
+        else
+        {
             anim.SetInteger("Walk", 0);
         }
 
         transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
 
-        if (Input.GetButtonDown("J" + player + "_Jump_360") && Time.time > canJump)
+        if (Input.GetButtonDown("J" + player + "_Jump_" + gPad) && Time.time > canJump)
         {
-                rb.AddForce(0, jumpForce, 0);
-                canJump = Time.time + timeBeforeNextJump;
-                anim.SetTrigger("jump");
+            rb.AddForce(0, jumpForce, 0);
+            canJump = Time.time + timeBeforeNextJump;
+            anim.SetTrigger("jump");
         }
         // linus added this
         // need some fixing
-        if (Input.GetButtonDown("J" + player + "_Light_360"))
+        if (Input.GetButtonDown("J" + player + "_Light_" + gPad))
         {
             anim.SetTrigger("Peck");
         }

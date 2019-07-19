80b8aa0f0ecb90ebd833824607a2370a139ffb2f
FerousFolly <tiddle97@gmail.com>
2019-07-19 10:50:00 +1000

    Actually the changes described last commit

--- Assets/_MyScripts/ChickenController.cs
+++ Assets/_MyScripts/ChickenController.cs
@@ -1,44 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class ChickenController : PlayerController
-{
+// public class ChickenController : PlayerController
+// {
 
-    protected Animator anim;
-    protected Rigidbody rb;
+//     protected Animator anim;
+//     protected Rigidbody rb;
 
-    void Start()
-    {
-        anim = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody>();
-    }
+//     void Start()
+//     {
+//         anim = GetComponent<Animator>();
+//         rb = GetComponent<Rigidbody>();
+//     }
 
-    // Update is called once per frame
-    void Update()
-    {
+//     // Update is called once per frame
+//     void Update()
+//     {
 
-        ControlPlayer();
-        //float h = CrossPlatformInputManager.GetAxis("J" + inputJoystick + "_Horizontal_" + gampadType);
-        //float v = CrossPlatformInputManager.GetAxis("J" + inputJoystick + "_Vertical_" + gampadType);
-        //jump = CrossPlatformInputManager.GetButton("J" + inputJoystick + "_Jump_" + gampadType);
-        //lightAttack = CrossPlatformInputManager.GetButton("J" + inputJoystick + "_Light_" + gampadType);
-        //heavyAttack = CrossPlatformInputManager.GetButton("J" + inputJoystick + "_Heavy_" + gampadType);
-        //specialAttack = CrossPlatformInputManager.GetButton("J" + inputJoystick + "_Special_" + gampadType);
-        //// linus added this
-        //// need some fixing
-        if (Input.GetButtonDown("J" + player + "_Light_" + gPad))
-        {
-            anim.SetTrigger("Peck");
+//         //ControlPlayer();
+//         //float h = CrossPlatformInputManager.GetAxis("J" + inputJoystick + "_Horizontal_" + gampadType);
+//         //float v = CrossPlatformInputManager.GetAxis("J" + inputJoystick + "_Vertical_" + gampadType);
+//         //jump = CrossPlatformInputManager.GetButton("J" + inputJoystick + "_Jump_" + gampadType);
+//         //lightAttack = CrossPlatformInputManager.GetButton("J" + inputJoystick + "_Light_" + gampadType);
+//         //heavyAttack = CrossPlatformInputManager.GetButton("J" + inputJoystick + "_Heavy_" + gampadType);
+//         //specialAttack = CrossPlatformInputManager.GetButton("J" + inputJoystick + "_Special_" + gampadType);
+//         //// linus added this
+//         //// need some fixing
+//         // if (Input.GetButtonDown("J" + player + "_Light_" + gPad)
+//         {
+//             anim.SetTrigger("Peck");
 
-        }
-        if (Input.GetButtonDown("J" + player + "_Special_" + gPad))
-        {
-            anim.SetTrigger("Special");
-        }
-        if (Input.GetButtonDown("J" + player + "_Heavy_" + gPad))
-        {
-            anim.SetTrigger("Heavy");
-        }
-    }
-}
+//         }
+//         if (Input.GetButtonDown("J" + player + "_Special_" + gPad))
+//         {
+//             anim.SetTrigger("Special");
+//         }
+//         if (Input.GetButtonDown("J" + player + "_Heavy_" + gPad))
+//         {
+//             anim.SetTrigger("Heavy");
+//         }
+//     }
+// }

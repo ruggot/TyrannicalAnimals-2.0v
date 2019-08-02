7e390f08ad8ed90d34b076b886cfe287070d5549
Andrew R <andrew.j.radford@outlook.com>
2019-08-02 12:42:32 +1000

    Camera polish, water fixed

--- Assets/_Scripts/CameraController.cs
+++ Assets/_Scripts/CameraController.cs
@@ -9,11 +9,13 @@ public class CameraController : MonoBehaviour
     [SerializeField] protected Transform player2;
 
     private Vector3 offset = new Vector3(0, 20, -20);
-
+    public float tweenSpeed = 3;
     private Vector3 lastOffset;
     private Vector3 midpoint;
+    private Vector3 newMidpoint;
     private float pDist;
     private Vector3 newCamPos;
+    private Vector3 n;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,9 +26,7 @@ public class CameraController : MonoBehaviour
     void Update()
     {
         UpdateVars();
-        {
-            UpdateCameraPos();
-        }
+        UpdateCameraPos();
         mainCamera.LookAt(midpoint);
     }
 
@@ -35,17 +35,27 @@ public class CameraController : MonoBehaviour
         pDist = Vector3.Distance(player1.position, player2.position);
         lastOffset = offset;
         offset.Set(offset.x, pDist / 3, -pDist);
-        midpoint = (player1.position + player2.position) / 2;
+        newMidpoint = (player1.position + player2.position) / 2;
+        midpoint = Tween(midpoint, newMidpoint);
     }
 
     void UpdateCameraPos()
     {
         // newCamPos = midpoint + offset;
-        if (Vector3.Distance(midpoint + offset, midpoint) < 10)
+        if (Vector3.Distance(midpoint + offset, midpoint) < 10 )
         {
             offset = lastOffset;
         }
         newCamPos = midpoint + offset;
-        mainCamera.SetPositionAndRotation(newCamPos, mainCamera.rotation);
+        // "Tweening" smooth movment effect
+        //newCamPos = Tween(mainCamera.position, newCamPos);
+        mainCamera.SetPositionAndRotation(newCamPos , mainCamera.rotation);
+    }
+
+    // "Tweening" smooth movment effect
+    Vector3 Tween(Vector3 current, Vector3 next)
+    {
+        n = current + (next - current) * (tweenSpeed * Time.fixedDeltaTime); //, current.y + (next.y - current.y) * (tweener * Time.fixedDeltaTime), current.z + (next.z - current.z) * (tweener * Time.fixedDeltaTime));
+        return n;
     }
 }
---
+++ Assets/_Scripts/TextureScrolling.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureScrolling : MonoBehaviour
+{
+    public float xSpeed = 1f;
+    public float ySpeed = 1f;
+    public Renderer rend;
+    private float curX;
+    private float curY;
+
+    void Awake()
+    {
+        rend = GetComponent<Renderer>();
+        curX = rend.material.mainTextureOffset.x;
+        curY = rend.material.mainTextureOffset.y;
+    }
+
+
+    void FixedUpdate()
+    {
+        curX += Time.deltaTime * (xSpeed / 10);
+        curY += Time.deltaTime * (ySpeed / 10);
+
+        rend.material.SetTextureOffset("_MainTex", new Vector2(curX, curY));
+    }
+}

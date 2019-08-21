c9d6e456ef3bfc40bb736a42c2ee24b9ffefec3f
FerousFolly <tiddle97@gmail.com>
2019-08-21 13:08:22 +1000

    gg all ree

--- Assets/_Scripts/CameraController.cs
+++ Assets/_Scripts/CameraController.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
-{
+public class CameraController : MonoBehaviour {
     [SerializeField] protected Transform mainCamera;
 
     private Vector3 offset = new Vector3(0, 20, -20);
@@ -17,21 +17,18 @@ public class CameraController : MonoBehaviour
     [SerializeField] GameObject player1;
     [SerializeField] GameObject player2;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         UpdateVars();
         UpdateCameraPos();
         mainCamera.LookAt(midpoint);
     }
 
-    void UpdateVars()
-    {
+    void UpdateVars() {
         pDist = Vector3.Distance(DataManager.Player(0).transform.position, DataManager.Player(1).transform.position);
         lastOffset = offset;
         offset.Set(offset.x, pDist / 3, -pDist);
@@ -39,22 +36,19 @@ public class CameraController : MonoBehaviour
         midpoint = Tween(midpoint, newMidpoint);
     }
 
-    void UpdateCameraPos()
-    {
+    void UpdateCameraPos() {
         // newCamPos = midpoint + offset;
-        if (Vector3.Distance(midpoint + offset, midpoint) < 10 )
-        {
+        if (Vector3.Distance(midpoint + offset, midpoint) < 10) {
             offset = lastOffset;
         }
         newCamPos = midpoint + offset;
         // "Tweening" smooth movment effect
         //newCamPos = Tween(mainCamera.position, newCamPos);
-        mainCamera.SetPositionAndRotation(newCamPos , mainCamera.rotation);
+        mainCamera.SetPositionAndRotation(newCamPos, mainCamera.rotation);
     }
 
     // "Tweening" smooth movment effect
-    Vector3 Tween(Vector3 current, Vector3 next)
-    {
+    Vector3 Tween(Vector3 current, Vector3 next) {
         n = current + (next - current) * (tweenSpeed * Time.fixedDeltaTime); //, current.y + (next.y - current.y) * (tweener * Time.fixedDeltaTime), current.z + (next.z - current.z) * (tweener * Time.fixedDeltaTime));
         return n;
     }
